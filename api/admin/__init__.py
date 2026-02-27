c8639762c74cbdef06898c687ac2e72a49f40700
Nikitchenko Nikita <nikitos13.n@gmail.com>
2026-02-27 22:38:26 +0400

    add textblock model in admin panel

--- api/admin/__init__.py
+++ api/admin/__init__.py
@@ -1,6 +1,9 @@
 from sqladmin import Admin
 from api.admin.news import NewsAdmin
+from api.admin.textblock import TextBlockView
+
 
 
 def register_admin_views(admin: Admin):
     admin.add_view(NewsAdmin)
+    admin.add_view(TextBlockView)
